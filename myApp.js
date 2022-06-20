060ba24dd2fd32da33366853a74e4f8eece17d63
Samson Ahadji <samahadji@gmail.com>
2022-06-20 23:14:00 +0000

    Fixing Mongoose connection

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 
 async function main() {
-  await mongoose.connect(process.env.MONGO_URI); //, { useNewUrlParser: true, useUnifiedTopology: true }
+  await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }); //, { useNewUrlParser: true, useUnifiedTopology: true }
 }
 
 let Person;
