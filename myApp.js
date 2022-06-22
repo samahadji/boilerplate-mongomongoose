fa2e8ba6bb1c8ab39ae0f42972921a553c5a997a
Samson Ahadji <samahadji@gmail.com>
2022-06-22 15:03:16 +0000

    Adding the Person schema and model

--- myApp.js
+++ myApp.js
@@ -1,12 +1,21 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-
 mongoose.connect(process.env.MONGO_URI); //, { useNewUrlParser: true, useUnifiedTopology: true }
 
 
-
 let Person;
+
+let personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide a string value"]
+  },
+  age: Number,
+  favoriteFoods : Array,
+})
+
+module.exports = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
